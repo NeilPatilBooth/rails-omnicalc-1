7dcb5b082f2d60abab2c6ef464c880a15dc6b19c
NeilPatilBooth <npatil@ChicagoBooth.edu>
2024-11-02 15:13:32 +0000

    square works

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  ##Home##
+  
+
   ##Square##
   get("/square/new", { :controller => "squarenew", :action =>"square_new"})
   get("/square/results", { :controller =>"squareresults", :action =>"square_results"})
